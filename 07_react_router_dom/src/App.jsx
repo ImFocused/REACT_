71a0aeb1907c4938ffc7aa384e722eeff34a1e6c
Pratham Jain <prathamjainpbj2306@gmail.com>
2026-04-03 12:12:13 +0530

    07_router project start

---
+++ 07_react_router_dom/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+
+import './App.css'
+import Header from "./components/Header/Header_temp";
+import Footer from './components/Footer/Footer'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <Header />
+    <h1 className="bg-green-600 p-4">React Router DOM</h1>
+    <Footer />
+    </>
+  )
+}
+
+export default App

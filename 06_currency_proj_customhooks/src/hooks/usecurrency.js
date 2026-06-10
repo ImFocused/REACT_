7ef50e36d35fa280b6b37326a4de7d738350bfac
Pratham Jain <prathamjainpbj2306@gmail.com>
2026-06-10 20:22:44 +0530

    currency converter proj dev ongoing

--- 06_currency_proj_customhooks/src/hooks/usecurrency.js
+++ 06_currency_proj_customhooks/src/hooks/usecurrency.js
@@ -1,16 +1,19 @@
 import { useEffect,useState } from "react";
 
 function useCurrencyinfo(currency){ 
-    useEffect(()=>{
+    useEffect(()=>{ //here useeffect is used to fetch the data from the api whenever the currency value changes
 
         const[data,setData] = useState({});
 
         fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`)
-        .then((res)=> res.json())   // convert the response to json format
+        .then((res)=> res.json())   // convert the response to json format as it comes in string format
         .then((res)=> setData(res[currency]))  // setdata is a function which will get the data from the api
         console.log(data);
        
 
     },[currency])   // currency is a dependency as we want to fetch the fn wheneveer the currency value changes
+    console.log(data);
+    return data;
 
 }
+export default useCurrencyinfo;
